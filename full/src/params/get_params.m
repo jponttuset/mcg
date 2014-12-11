f0e24432c0c35ca39809d9dde9d3653cd7e10446
Jordi Pont-Tuset <jponttuset@gmail.com>
2014-12-11 13:50:02 +0100

    - Default parameters restored

---
+++ full/src/params/get_params.m
@@ -0,0 +1,19 @@
+% ------------------------------------------------------------------------ 
+%  Copyright (C)
+%  Universitat Politecnica de Catalunya BarcelonaTech (UPC) - Spain
+%  University of California Berkeley (UCB) - USA
+% 
+%  Jordi Pont-Tuset <jordi.pont@upc.edu>
+%  Pablo Arbelaez <arbelaez@berkeley.edu>
+%  June 2014
+% ------------------------------------------------------------------------ 
+% This file is part of the MCG package presented in:
+%    Arbelaez P, Pont-Tuset J, Barron J, Marques F, Malik J,
+%    "Multiscale Combinatorial Grouping,"
+%    Computer Vision and Pattern Recognition (CVPR) 2014.
+% Please consider citing the paper if you use this code.
+% ------------------------------------------------------------------------
+function params = get_params()
+    params = sf_mUCM_multi_3sc_u_4r_12k_params;
+end
+
