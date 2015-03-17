50a2e13af1869eb25a12864282a3cdcd77efa905
Jordi Pont-Tuset <jponttuset@gmail.com>
2015-03-17 15:24:28 +0100

    - Updating pre-trained

--- pre-trained/datasets/database_root_dir.m
+++ pre-trained/datasets/database_root_dir.m
@@ -13,17 +13,16 @@
 %    Computer Vision and Pattern Recognition (CVPR) 2014.
 % Please consider citing the paper if you use this code.
 % ------------------------------------------------------------------------
-% Adapt the paths to the place where you have downloaded PASCAL and BSDS.
-% In the case of BSDS500, you should put all images and ground truths 
-% into the same folder, discarding the "val", "test", "train" folders
+% Adapt the paths to the place where you have downloaded PASCAL.
 % ------------------------------------------------------------------------
+
 function db_root_dir = database_root_dir( database )
 if strcmp(database,'pascal2012')
-    db_root_dir = '/path/to/PASCAL2012/';
-elseif strcmp(database,'bsds500')
-    db_root_dir = '/Users/jpont/Workspace/gt_dbs/BSDS500/';
+    db_root_dir ='/path/to/pascal2012';
 elseif strcmp(database,'COCO')
-    db_root_dir = '/Users/jpont/Workspace/gt_dbs/COCO/';
+    db_root_dir = '/path/to/COCO/';
+elseif strcmp(database,'SBD')
+    db_root_dir = '/path/to/SBD/';
 else
     error(['Unknown database: ' database]);
 end
