b321f63e7940b57906947def013577afe0fa6838
Jordi Pont-Tuset <jponttuset@gmail.com>
2015-05-19 10:00:21 +0200

    Revert "- Scores improved"
    This reverts commit 5f9db350c2611e6cc572a647d0d1aabc99174db5.

--- pre-trained/im2mcg.m
+++ pre-trained/im2mcg.m
@@ -13,34 +13,32 @@
 %    Computer Vision and Pattern Recognition (CVPR) 2014.
 % Please consider citing the paper if you use this code.
 % ------------------------------------------------------------------------
-% This function computes the MCG proposals given an image.
+% This function computes the MCG candidates given an image.
 %  INPUT:
 %  - image : Input image
 %  - mode  : It can be: + 'fast'     (SCG in the paper)
 %                       + 'accurate' (MCG in the paper)
-%  - compute_masks : Compute the proposals maks [0] or not [1]. Note that
+%  - compute_masks : Compute the candidates maks [0] or not [1]. Note that
 %                    it is very time consuming. Otherwise use the labels 
 %                    as shown in the demo.
 %
 %  OUTPUT:
-%  - proposals : Struct containing the following fields
-%          + superpixels   : Label matrix of the superpixel partition
-%          + labels        : Cell containing the superpixel labels that form
-%                            each of the proposals
-%          + scores        : Score of each of the ranked proposals
-%          + masks         : 3D boolean matrix containing the masks of the proposals
-%                            (only if compute_masks==1)
-%          + bboxes        : Bounding boxes of the proposals (up,left,down,right)
-%                            See 'bboxes' folder for functions to work with them
-%          + bboxes_scores : Score of each of the ranked bounding box
-%
-%  - ucm2       : Ultrametric Contour Map from which the proposals are
+%  - candidates : Struct containing the following fields
+%          + superpixels : Label matrix of the superpixel partition
+%          + labels : Cell containing the superpixel labels that form
+%                     each of the candidates
+%          + scores : Score of each of the ranked candidates
+%          + masks  : 3D boolean matrix containing the masks of the candidates
+%                     (only if compute_masks==1)
+%  - ucm2       : Ultrametric Contour Map from which the candidates are
 %                 extracted
+%  - bboxes     : Bounding boxes of the candidates (up,left,down,right)
+%                 See 'bboxes' folder for functions to work with them
 %
 %  DEMO:
 %  - See demos/demo_im2mcg.m
 % ------------------------------------------------------------------------
-function [proposals, ucm2, times] = im2mcg(image,mode,compute_masks)
+function [candidates, ucm2, times] = im2mcg(image,mode,compute_masks)
 if nargin<2
     mode = 'fast';
 end
@@ -68,7 +66,7 @@ if strcmp(mode,'fast')
     % Load pre-trained pareto point
     pareto_n_cands = loadvar(fullfile(mcg_root, 'datasets', 'models', 'scg_pareto_point_train2012.mat'),'n_cands');
 
-    % Load pre-trained random forest regresssor for the ranking of proposals
+    % Load pre-trained random forest regresssor for the ranking of candidates
     rf_regressor = loadvar(fullfile(mcg_root, 'datasets', 'models', 'scg_rand_forest_train2012.mat'),'rf');
 
 elseif strcmp(mode,'accurate')
@@ -82,7 +80,7 @@ elseif strcmp(mode,'accurate')
     % Load pre-trained pareto point
     pareto_n_cands = loadvar(fullfile(mcg_root, 'datasets', 'models', 'mcg_pareto_point_train2012.mat'),'n_cands');
 
-    % Load pre-trained random forest regresssor for the ranking of proposals
+    % Load pre-trained random forest regresssor for the ranking of candidates
     rf_regressor = loadvar(fullfile(mcg_root, 'datasets', 'models', 'mcg_rand_forest_train2012.mat'),'rf');
 else
     error('Unknown mode for MCG: Possibilities are ''fast'' or ''accurate''')
@@ -106,7 +104,7 @@ end
 % Get full cands, represented on a fused hierarchy
 [f_lp,f_ms,cands,start_ths,end_ths] = full_cands_from_hiers(lps,ms,ths,pareto_n_cands);
 
-% Hole filling and complementary proposals
+% Hole filling and complementary candidates
 if ~isempty(f_ms)
     [cands_hf, cands_comp] = hole_filling(double(f_lp), double(f_ms), cands); %#ok<NASGU>
 else
@@ -114,8 +112,8 @@ else
     cands_comp = cands; %#ok<NASGU>
 end
 
-% Select which proposals to keep (Uncomment just one line)
-cands = cands_hf;                       % Just the proposals with holes filled
+% Select which candidates to keep (Uncomment just one line)
+cands = cands_hf;                       % Just the candidates with holes filled
 % cands = [cands_hf; cands_comp];         % Holes filled and the complementary
 % cands = [cands; cands_hf; cands_comp];  % All of them
         
@@ -131,7 +129,7 @@ red_cands = mex_fast_reduction(cands-1,b_feats.areas,b_feats.intersections,J_th)
 % Compute full features on reduced cands
 [feats, bboxes] = compute_full_features(red_cands,b_feats);
 
-% Rank proposals
+% Rank candidates
 class_scores = regRF_predict(feats,rf_regressor);
 [scores, ids] = sort(class_scores,'descend');
 red_cands = red_cands(ids,:);
@@ -141,30 +139,31 @@ if isrow(scores)
 end
 
 % Max margin
-[new_ids, proposals.scores] = mex_max_margin(red_cands-1,scores,b_feats.intersections,theta);
+[new_ids, mm_scores] = mex_max_margin(red_cands-1,scores,b_feats.intersections,theta); %#ok<NASGU>
 cand_labels = red_cands(new_ids,:);
+candidates.scores = scores(new_ids);
 bboxes = bboxes(new_ids,:); 
 
 % Filter boxes by overlap
-[red_bboxes, proposals.bboxes_scores] = mex_box_reduction(bboxes, proposals.scores, 0.95);
+red_bboxes = mex_box_reduction(bboxes, 0.95);
 
 % Change the coordinates of bboxes to be coherent with
 % other results from other sources (sel_search, etc.)
-proposals.bboxes = [red_bboxes(:,2) red_bboxes(:,1) red_bboxes(:,4) red_bboxes(:,3)];
+candidates.bboxes = [red_bboxes(:,2) red_bboxes(:,1) red_bboxes(:,4) red_bboxes(:,3)];
 
-% Get the labels of leave regions that form each proposals
-proposals.superpixels = f_lp;
+% Get the labels of leave regions that form each candidates
+candidates.superpixels = f_lp;
 if ~isempty(f_ms)
-    proposals.labels = cands2labels(cand_labels,f_ms);
+    candidates.labels = cands2labels(cand_labels,f_ms);
 else
-    proposals.labels = {1};
+    candidates.labels = {1};
 end
 
 % Transform the results to masks
 if compute_masks
     if ~isempty(f_ms)
-        proposals.masks = cands2masks(cand_labels, f_lp, f_ms);
+        candidates.masks = cands2masks(cand_labels, f_lp, f_ms);
     else
-        proposals.masks = true(size(f_lp));
+        candidates.masks = true(size(f_lp));
     end
 end
