2fecc6a283edd022d24dd55f29c34af2dac04130
Jordi Pont-Tuset <jponttuset@gmail.com>
2016-11-30 22:27:43 +0100

    - JSON adapted

--- benchmark/src/boxes/eval_boxes_list.m
+++ benchmark/src/boxes/eval_boxes_list.m
@@ -25,40 +25,55 @@ for ii=1:length(ids)
     if ~exist(res_file, 'file')
           
         % Check if boxes are computed
-        input_file = fullfile(input_dir,[curr_id '.mat']);
+        input_file = fullfile(prop_dir,[curr_id '.mat']);
         if ~exist(input_file, 'file')
-            input_file = fullfile(input_dir,[curr_id '_masks.mat']);
+            input_file = fullfile(prop_dir,[curr_id '_masks.mat']);
             if ~exist(input_file, 'file')
-                input_file = fullfile(input_dir,[curr_id '.txt']);                
+                input_file = fullfile(prop_dir,[curr_id '.json']);
                 if ~exist(input_file, 'file')
-                    error(['Results ''' input_file '/mat''not found. Have you computed them?'])
+                    input_file = fullfile(prop_dir,[curr_id '.txt']);
+                    if ~exist(input_file, 'file')
+                        error(['Results ''' input_file '/mat/json''not found. Have you computed them?'])
+                    end
                 end
             end
         end
         
         % Load boxes
-        tmp = load(input_file);
-        if isfield(tmp,'boxes')
-            boxes = tmp.boxes;
-        elseif strcmp(method_name,'EB')
-            boxes = [tmp.boxes(:,2), tmp.boxes(:,1), tmp.boxes(:,2)+tmp.boxes(:,4), tmp.boxes(:,1)+tmp.boxes(:,3)];
-        elseif strcmp(method_name,'SeSe')
-            boxes = tmp.boxes;
-        elseif isfield(tmp,'masks')
-            boxes = zeros(size(tmp.masks,3),4);
-            for jj=1:size(tmp.masks,3)
-                boxes(jj,:) = mask2box(tmp.masks(:,:,jj));
-            end
-        elseif isfield(tmp,'superpixels')
-            boxes = labels2boxes(tmp.superpixels, tmp.labels);
-        elseif isnumeric(tmp)
-            boxes = [tmp(:,2), tmp(:,1), tmp(:,4), tmp(:,3)];
-        elseif strcmp(method_name,'Obj') || strcmp(method_name,'RP')
-            boxes = round([tmp.boxes(:,2), tmp.boxes(:,1), tmp.boxes(:,4), tmp.boxes(:,3)]);
-            boxes(:,boxes(:,1)<1) = 1;
-            boxes(:,boxes(:,2)<1) = 1;
+        [~,method_name] = fileparts(strip(prop_dir,'right',filesep));
+        
+        [~,~,ext] = fileparts(input_file);
+        if strcmp(ext,'.json')
+            % Get and sort proposals
+            proposals = gason(fileread(input_file));
+            [~,s_id] = sort([proposals.score],'descend');
+            proposals = proposals(s_id);
+            boxes = reshape([proposals.bbox],[4,length(proposals)])';
+            % CONTINUE HERE: 0s? Size?
         else
-            error('Format not recognized');
+            tmp = load(input_file);
+            if strcmp(method_name,'EB')
+                boxes = [tmp.boxes(:,2), tmp.boxes(:,1), tmp.boxes(:,2)+tmp.boxes(:,4), tmp.boxes(:,1)+tmp.boxes(:,3)];
+            elseif strcmp(method_name,'SeSe')
+                boxes = tmp.boxes;
+            elseif isfield(tmp,'masks')
+                boxes = zeros(size(tmp.masks,3),4);
+                for jj=1:size(tmp.masks,3)
+                    boxes(jj,:) = mask2box(tmp.masks(:,:,jj));
+                end
+            elseif isfield(tmp,'superpixels')
+                boxes = labels2boxes(tmp.superpixels, tmp.labels);
+            elseif isnumeric(tmp)
+                boxes = [tmp(:,2), tmp(:,1), tmp(:,4), tmp(:,3)];
+            elseif strcmp(method_name,'Obj') || strcmp(method_name,'RP')
+                boxes = round([tmp.boxes(:,2), tmp.boxes(:,1), tmp.boxes(:,4), tmp.boxes(:,3)]);
+                boxes(:,boxes(:,1)<1) = 1;
+                boxes(:,boxes(:,2)<1) = 1;
+            elseif isfield(tmp,'boxes')
+                boxes = tmp.boxes;
+            else
+                error('Format not recognized');
+            end
         end
         
         % Load GT
@@ -82,14 +97,15 @@ for ii=1:length(ids)
         end
         
         % Save
-        parsave(res_file,jaccards,gt.category);
+        parsave(res_file,jaccards,gt.category,gt.obj_id,gt.im_id);
     end
 end
 end
 
 
-function parsave(res_file,jaccards,obj_classes) %#ok<INUSD>
-    save(res_file, 'jaccards', 'obj_classes');
+function parsave(res_file,jaccards,obj_classes,obj_ids,image_ids) %#ok<INUSD>
+    save(res_file, 'jaccards','obj_classes','obj_ids','image_ids');
 end
+
 
 
